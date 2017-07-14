8cc0520d5cb465a2d2553750fdd2ef5f0f8efb44
tylercartergse2013 <tylercartergse2013@gmail.com>
2017-07-14 12:23:52 -0400

    Edited script file to convert .md types to different file types.

--- tylerpcarter-convert-docs.sh
+++ tylerpcarter-convert-docs.sh
@@ -1,13 +1,20 @@
-
+#!/bin/bash
 
 # Declare input argument as a variable
+file=replication-and-decline
 
 # Convert markdown to HTML
+pandoc -o $file.html $file.md
+
 
 # Convert markdown to DOCX
+pandoc -o $file.docx $file.md
 
 # Convert markdown to ODT
+pandoc -o $file.docx $file.md
 
 # Convert markdown to PDF
+pandon -o $file.pdf $file.md
 
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
+echo "Coverted "$file" to HTML, DOCX, ODT, PDF, and insertFileTypeHere"
