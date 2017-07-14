83e4035122e42dee8a42cbdcd7162545d5a05186
tylercartergse2013 <tylercartergse2013@gmail.com>
2017-07-14 12:33:48 -0400

    Added Latex file conversion.

--- tylerpcarter-convert-docs.sh
+++ tylerpcarter-convert-docs.sh
@@ -4,17 +4,24 @@
 file=replication-and-decline
 
 # Convert markdown to HTML
-pandoc -o $file.html $file.md
-
+echo "Converting paper to HTML document."
+pandoc -S -o $file.html $file.md
 
 # Convert markdown to DOCX
-pandoc -o $file.docx $file.md
+echo "Converting paper to DOCX document."
+pandoc -S -o $file.docx $file.md
 
 # Convert markdown to ODT
-pandoc -o $file.docx $file.md
+echo "Converting paper to ODT document."
+pandoc -S -o $file.odt $file.md
 
 # Convert markdown to PDF
-pandon -o $file.pdf $file.md
+echo "Converting paper to PDF document."
+pandoc -S -o $file.pdf $file.md
+
+#Converting markdown to LATEX
+echo "Converting paper to LATEX document."
+pandoc -S -o $file.tex $file.md
 
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
 echo "Coverted "$file" to HTML, DOCX, ODT, PDF, and insertFileTypeHere"
